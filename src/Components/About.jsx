d7997fed51663d69a7bfab39d283017c465ba617
theranjitraut <rautranjit916@gmail.com>
2024-03-06 07:39:31 +0545

    commiting changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/earthimage.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "This is the Earth.";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a practitioner of Science, Mathematics, Technology, and Philosophy.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Artificial Intelligence",
+  "Machine Learning",
+  "Neural Network",
+  "Deep Learning",
+  "Natural Language Processing",
+  "Computer Vision",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about innovation, imagination, and technology. I always find a new way to learn more about them.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/oceanimage.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "This is the Ocean.";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My blog site",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Collaboration to create a beginner friendly article to help explain about technology and science to engage on.",
+    url: "https://www.surftheknowledge.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Text editing desktop application",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created using Python",
+    url: "https://github.com/theranjitraut/TxtEdit",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Created by me and deployed to GitHub pages. Includes my experience and design abilities.",
+    url: "https://www.ranjitraut.com.np",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Music Player",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created using Python",
+    url: "https://github.com/theranjitraut/MP3",
   },
 ];
 
